6944f832e155bcf6760981b0cc8c4e146cea4512
Yaroslav Snigur <yaroslavsnigur@Yaroslavs-MacBook-Air.local>
2022-09-18 15:47:06 -0400

    auth context set up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const currentUser = false;
+  const { currentUser } = useContext(AuthContext);
 
   const RequireAuth = ({ children }) => {
     return currentUser ? children : <Navigate to="/login" />;
   };
+
+  console.log(currentUser);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="login" element={<Login />} />
-          <Route index element={<Home />} />
+          <Route
+            index
+            element={
+              <RequireAuth>
+                <Home />
+              </RequireAuth>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
