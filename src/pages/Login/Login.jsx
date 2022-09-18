133bd0e506c8b001286088d9f9521c4c959a5046
Yaroslav Snigur <yaroslavsnigur@Yaroslavs-MacBook-Air.local>
2022-09-18 13:22:58 -0400

    login page created

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <form>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+        <span>Wrong email or password!</span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
